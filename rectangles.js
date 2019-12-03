b1d12d2a4224ea1e314df88c6bab0f5dade314a5
hadz333 <dhadzic@uw.edu>
2019-12-03 14:18:18 -0800

    sort choice implemented and connected to respective javascript functions

--- rectangles.js
+++ rectangles.js
@@ -3,21 +3,61 @@ var c = document.getElementById("myCanvas");
 var rectangleSlider = document.getElementById("rectangleSlider");
 var speedSlider = document.getElementById("speedSlider");
 
-var values = [];
-let num = rectangleSlider.value;
-for (i = 0; i < num; i++) {
-	// populate the array with values between 20 and 200 for rectangle size
-	values[i] = Math.floor(Math.random() * 180) + 20
+
+generateRectangles(5);
+
+function updateSlider(newValue) {
+	generateRectangles(newValue);
 }
 
-var y = 120;
-var x = 0;
-var rectWidth = 15;
-var ctx = c.getContext("2d");
-ctx.beginPath();
-for (i = 0; i < values.length; i++) {
-	ctx.rect(x, y, rectWidth, values[i]);
-	x += rectWidth;
+
+
+function generateRectangles(numRectangles) {
+	var values = [];
+	for (i = 0; i < numRectangles; i++) {
+		// populate the array with values between 20 and 200 for rectangle size
+		values[i] = Math.floor(Math.random() * 180) + 20
+	}
+
+	var y = 0;
+	var x = 0;
+	var rectWidth = 15;
+	var ctx = c.getContext("2d");
+	ctx.clearRect(0, 0, c.width, c.height);
+	ctx.beginPath();
+	for (i = 0; i < values.length; i++) {
+		ctx.rect(x, y, rectWidth, values[i]);
+		x += rectWidth;
+	}
+
+	ctx.stroke();
 }
 
-ctx.stroke();
+// chooses the kind of sort to run
+function chooseSort() {
+	if (document.getElementById('merge').checked) {
+  		mergeSort();
+	} else if (document.getElementById('quick').checked) {
+  		quickSort();
+	} else if (document.getElementById('heap').checked) {
+		heapSort();
+	} else if (document.getElementById('bubble').checked) {
+		bubbleSort();
+	}
+}
+
+function mergeSort() {
+	console.log("merge");
+}
+
+function quickSort() {
+	console.log("quick");
+}
+
+function heapSort() {
+	console.log("heap");
+}
+
+function bubbleSort() {
+	console.log("bubble");
+}
