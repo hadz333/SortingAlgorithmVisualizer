ebe544e96069a8888b7f5e691607d52dc0add5e7
hadz333 <dhadzic@uw.edu>
2019-12-03 16:40:11 -0800

    bubble sort and sleep implemented

--- rectangles.js
+++ rectangles.js
@@ -1,10 +1,13 @@
 
 var c = document.getElementById("myCanvas");
+var ctx = c.getContext("2d");
+ctx.fillStyle = "#6395e6";
 var rectangleSlider = document.getElementById("rectangleSlider");
 var speedSlider = document.getElementById("speedSlider");
 
-
-generateRectangles(5);
+var values = [];
+var numRectangles = 5;
+generateRectangles(numRectangles);
 
 function updateSlider(newValue) {
 	generateRectangles(newValue);
@@ -13,24 +16,40 @@ function updateSlider(newValue) {
 
 
 function generateRectangles(numRectangles) {
-	var values = [];
+	this.numRectangles = numRectangles;
+	values = [];
 	for (i = 0; i < numRectangles; i++) {
 		// populate the array with values between 20 and 200 for rectangle size
 		values[i] = Math.floor(Math.random() * 180) + 20
 	}
 
+	redraw();
+}
+
+function redraw() {
 	var y = 0;
 	var x = 0;
 	var rectWidth = 15;
-	var ctx = c.getContext("2d");
+
 	ctx.clearRect(0, 0, c.width, c.height);
 	ctx.beginPath();
 	for (i = 0; i < values.length; i++) {
-		ctx.rect(x, y, rectWidth, values[i]);
-		x += rectWidth;
+		ctx.fillRect(x, y, rectWidth, values[i]);
+		x += rectWidth + 2;
 	}
 
 	ctx.stroke();
+}
+
+function swapRectangles(i, j) {
+	// 17 is rectangle width + space between rectangles
+	var x1 = 17 * i;
+	var x2 = 17 * j;
+	ctx.clearRect(x1, 0, 15, c.height);
+	ctx.clearRect(x2, 0, 15, c.height);
+	ctx.fillRect(x1, 0, 15, values[i]);
+	ctx.fillRect(x2, 0, 15, values[j]);
+	//ctx.stroke();
 }
 
 // chooses the kind of sort to run
@@ -58,6 +77,23 @@ function heapSort() {
 	console.log("heap");
 }
 
-function bubbleSort() {
+async function bubbleSort() {
 	console.log("bubble");
+	var n = values.length;
+	for (let i = 0; i < n - 1; i++) {
+		for (let j = 0; j < n - i - 1; j++) {
+			if (values[j] > values[j + 1]) {
+				// swap the two values
+				let temp = values[j];
+				values[j] = values[j + 1];
+				values[j + 1] = temp;
+				swapRectangles(j, j + 1);
+				await sleep(500);
+			}
+		}
+	}
+}
+
+const sleep = (milliseconds) => {
+  return new Promise(resolve => setTimeout(resolve, milliseconds))
 }
